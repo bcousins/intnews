5360e640b9dff87f34f6bdc192ffc0b2c3e19fbc
Ben Cousins <ben.cousins8496@gmail.com>
2013-03-19 13:14:49 +1100

    fix for the fix I just commited :/

--- header.php
+++ header.php
@@ -41,7 +41,7 @@ function head() {
 	}
 	echo "<link rel=\"alternate\" type=\"application/rss+xml\" title=\"RSS\" href=\"backend.php\">\n";
 	echo "<LINK REL=\"StyleSheet\" HREF=\"themes/$ThemeSel/style/style.css\" TYPE=\"text/css\">\n";
-	echo "<link rel=\"stylesheet\" href=\"themes/DeepBlue/forums/{T_HEAD_STYLESHEET}\" type=\"text/css\">\n\n";
+	echo "<link rel=\"stylesheet\" href=\"themes/DeepBlue/forums/subSilver.css\" type=\"text/css\">\n\n";
 	if (file_exists("includes/custom_files/custom_head.php")) {
 		include_once("includes/custom_files/custom_head.php");
 	}
